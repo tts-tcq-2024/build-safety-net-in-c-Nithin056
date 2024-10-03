a661114556cd93e5febbd00f5caf4a04a1b9a64c
Nithin056 <nithinpro777@gmail.com>
2024-10-03 10:41:14 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -4,88 +4,63 @@
 #include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
-char R(char c){
-    if(c=='R'){
-        return '6';
+
+char getSoundexCode(char c) 
+{
+    static const char codeMap[26] = 
+    {
+        '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '0', '0', '2', '0', '2'
+    };
+    if (isalpha(c)) 
+    {
+        return codeMap[toupper(c) - 'A'];
     }
     return '0';
 }
-
-char MN(char c){
-    if(c=='N' || c=='M'){
-        return '5';
+void CheckAndAddCharacter(char *soundex, int *sIndex, char code, char *prevCode) 
+{ 
+    if (code != '0' && code != *prevCode) 
+    {
+        soundex[(*sIndex)++] = code;
+        *prevCode = code;
     }
-    return R(c);
 }
-
-char L(char c){
-    if(c=='L'){
-        return'4';
+ 
+void RemainingChars(const char *name, char *soundex, int *sIndex) 
+{
+    char prevCode = getSoundexCode(soundex[0]);
+    for (int i = 1; name[i] != '\0' && *sIndex < 4; i++) 
+    {
+        char code = getSoundexCode(name[i]);
+        CheckAndAddCharacter(soundex, sIndex, code, &prevCode);
     }
-   return MN(c);
 }
-
-char DT(char c){
-    if(c=='D' || c=='T'){
-        return '3';
-    }
-    return L(c);
-}
-
-char CGKKQSXZ(char c){
-    char ch[] = {'C','G','J','K','Q','S','X','Z'};
-    for(int i=0; i<8;i++){
-        if(c==ch[i]){
-            return '2';
-        }
-    }
-    return  DT(c);
-}
-
-char BFPV(char c){
-    char ch[]={'B','F','P','V'};
-    for(int i=0;i<4;i++){
-        if(c==ch[i]){
-            return '1';
-        }
-    }
-    return CGKKQSXZ(c);
-}
-
-char getSoundexCode(char c) {
-    c = toupper(c);
-    return BFPV(c);
-    
-}
-
-bool padding(int sIndex, char *soundex){
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
+ 
+ 
+void paddingWithZeros(char *soundex, int startIndex) 
+{
+    while (startIndex < 4) 
+    {
+        soundex[startIndex++] = '0';
     }
     soundex[4] = '\0';
-    return true;
 }
-
-bool soundex_check(int sIndex, char *soundex,const char *name,char code){
-    
-    if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
-    return true;
+ 
+void generateSoundex(const char *name, char *soundex)
+{
+    if (name[0] == '\0') 
+    {
+        strcpy(soundex, "0000");
+        return;
     }
-}
-
-void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
 
-    for (int i = 1; i < len && sIndex < 4; i++) {
-        char code = getSoundexCode(name[i]);
-        soundex_check(sIndex,soundex,name,code);
-    }
-
-    padding(sIndex,soundex);
-    
+ 
+    RemainingChars(name, soundex, &sIndex);
+    paddingWithZeros(soundex, sIndex);    
 }
+ 
+#endif // SOUNDEX_H
 
 #endif // SOUNDEX_H
